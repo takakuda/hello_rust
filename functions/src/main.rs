caafec6637647e0622bd1d99707a0c0da6391942
takakuda <kei.takakuda.0415@icloud.com>
2023-12-11 17:10:06 +0900

    value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-
-    another_function(5);
+fn five() -> i32 {
+    5
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {}", x);
+fn main() {
+    let x = five();
+
+    println!("The value of x is : {}", x);
 }
